2acae59ce642619d0b33c9a95c547854334b1a92
25021695-PhamDangDuy <25021695@vnu.edu.vn>
2025-10-01 23:48:42 +0700

    Update Ex4.py

--- Ex_W3/Ex4.py
+++ Ex_W3/Ex4.py
@@ -6,7 +6,7 @@ if (len(user_input) != 1):
 if (  (user_input != user_input.upper()) or (user_input != user_input.lower()) ):   #Nguyên lí: nếu kí tự nhập vào là 1 chữ cái thì sẽ có dạng in hoa - in thường và ngược lại nếu đó là số / kí hiệu thì chỉ có 1 dạng duy nhất
     print(f"{user_input} là chữ cái alphabet")
 else:
-    print(f"{user_input} không phải chữ cái alphabet")
+    print(f"{user_input} không phải chữ cái alphabet đâu nha cưng")
 
 #Cách 2: dùng hàm isalpha()
 user_input = input("Nhập vào kí tự cần kiểm tra: ")
